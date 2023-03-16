453489fbd3ecceddf53a5e6f9e9598872eee8f76
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-03-16 14:23:21 +0500

    Added Axios in Movie Details + Styling

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,13 +1,54 @@
 <template>
     <div class="movie-detail">
-        Detail {{ $route.params.id }}
+        <h2>{{this.movie.Title}}</h2>
+        <p>{{ this.movie.Year }}</p>
+        <img :src="this.movie.Poster" alt="Movie Poster" class="featured-img">
+        <p>{{ movie.Plot }}</p>
     </div>
 </template>
 
 <script>
+import { useRoute } from 'vue-router'
+import env from '@/env';
+import axios from 'axios';
+export default {
+    
+    data() {
+        return {
+            movie: [],
+            route: useRoute(),
+        }
+    },
+
+    beforeMount() {
+        axios.get(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${this.route.params.id}&plot=full`)
+        .then( response => {
+            this.movie = response.data
+        })
+        
+    }
+}
 </script>
 
-<style>
-
+<style lang="scss">
+.movie-detail {
+  padding: 16px;
+  h2 {
+    color: #FFF;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+  p {
+    color: #FFF;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
 </style>
 
